0ec947681e3c7fb82d7e80a7e68edd10053d9e46
Lucas Schvindt <lucas.schvindt@gmail.com>
2023-08-16 12:15:15 -0400

    Created an Apollo Provider ro make every request work with the Apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,33 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+// Create an http link to connect to your Apollo server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+  headers: {
+    authorization: localStorage.getItem('token') || '',
+  },
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
